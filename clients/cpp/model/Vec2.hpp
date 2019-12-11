5cf67472a5b826741c6f45f19c58c328c876d716
lookuut <lookuut@gmail.com>
2019-12-11 16:39:49 +0300

    Add Vec2

---
+++ clients/cpp/model/Vec2.hpp
@@ -0,0 +1,37 @@
+//
+// Created by lookuut on 11.12.19.
+//
+
+#ifndef AICUP2019_VEC2_HPP
+#define AICUP2019_VEC2_HPP
+
+#include "../Stream.hpp"
+#include <string>
+#include <math.h>
+
+class Vec2 {
+public:
+
+    virtual void writeTo(OutputStream& stream) const =0;
+
+    virtual double distSqr(const Vec2 & point) const =0;
+
+    virtual std::string toString() const =0;
+
+    virtual Vec2 * operator+(const Vec2 & vec) const =0;
+    virtual Vec2 * operator-(const Vec2 & vec) const =0;
+
+    virtual void operator+=(const Vec2 & vec) =0;
+    virtual void operator-=(const Vec2 & vec) =0;
+    virtual bool operator==(const Vec2 & vec) =0;
+    virtual bool operator!=(const Vec2 & vec) =0;
+
+    virtual void operator*=(double f) =0;
+
+    virtual double len() const =0;
+    virtual double sqrLen() const =0;
+    virtual Vec2& normalize() =0;
+    virtual Vec2 * rotate(double angle) const =0;
+    virtual Vec2 * toFloat() =0;
+};
+#endif //AICUP2019_VEC2_HPP
